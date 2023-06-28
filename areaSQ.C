698dd4cf8380987fcd91289ddc3fe0cb4adf47be
saharathp <126144646+saharathp@users.noreply.github.com>
2023-06-28 22:02:55 +0545

    Add files via upload

---
+++ areaSQ.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int a;
+    printf("Enter the value of side:\n");
+    scanf("%d",&a);
+    printf("The value of area is %f",pow(a,2));
+    return 0;
+}
